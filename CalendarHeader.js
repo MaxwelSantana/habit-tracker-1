657b1c189e468551a28a99fceec3d76b49a8cc2c
MaxwelSantana <maxwelsant96@icloud.com>
2021-01-19 16:59:06 -0300

    testing react-native-calendar-strip

--- CalendarHeader.js
+++ CalendarHeader.js
@@ -1,12 +1,23 @@
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
 import DefaultText from './DefaultText';
+import CalendarStrip from 'react-native-calendar-strip';
 
+//https://github.com/BugiDev/react-native-calendar-strip
 export default function CalendarHeader() {
     return (
         <View style={styles.container}>
             <DefaultText style={styles.selectedDay}>1 de Janeiro</DefaultText>
-            <View style={styles.calendar}></View>
+            <CalendarStrip
+                scrollable
+                style={{ height: 50 }}
+                calendarColor={'#3343CE'}
+                calendarHeaderStyle={{ color: 'white' }}
+                dateNumberStyle={{ color: 'white' }}
+                dateNameStyle={{ color: 'white' }}
+                iconContainer={{ flex: 0.1 }}
+                showMonth={false}
+            />
         </View>
     );
 }
